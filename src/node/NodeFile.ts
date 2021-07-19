60cba595b7529cf4cb8285edced4742eb2109f3a
Yoshihiro Seki <nikochan2k@gmail.com>
2021-07-19 23:11:39 +0900

    refactoring

--- src/node/NodeFile.ts
+++ src/node/NodeFile.ts
@@ -1,10 +1,9 @@
-import { createHash } from "crypto";
 import * as fs from "fs";
 import { AbstractFile } from "../core/AbstractFile";
 import { AbstractFileSystem } from "../core/AbstractFileSystem";
 import { AbstractReadStream } from "../core/AbstractReadStream";
 import { AbstractWriteStream } from "../core/AbstractWriteStream";
-import { DeleteOptions, OpenOptions, OpenWriteOptions } from "../core/core";
+import { DeleteOptions, OpenWriteOptions } from "../core/core";
 import { joinPaths } from "../util/path";
 import { convertError } from "./NodeFileSystem";
 import { NodeReadStream } from "./NodeReadStream";
@@ -42,49 +41,6 @@ export class NodeFile extends AbstractFile {
           }
         }
       );
-    });
-  }
-
-  public override hash(options: OpenOptions = {}): Promise<string> {
-    return new Promise<string>(async (resolve, reject) => {
-      const repository = this.fs.repository;
-      const path = this.path;
-      if (!options.ignoreHook) {
-        const beforeGet = this.fs.options.hook?.beforeGet;
-        if (beforeGet) {
-          await beforeGet(path, options);
-        }
-      }
-      const hash = createHash("sha256");
-      try {
-        var input = fs.createReadStream(this.getFullPath(), {
-          highWaterMark: options.bufferSize,
-        });
-      } catch (e) {
-        throw convertError(repository, path, e, false);
-      }
-      const onData = (data: any) => {
-        hash.update(data);
-      };
-      input.on("data", onData);
-      const onEnd = async () => {
-        if (!options.ignoreHook) {
-          const afterGet = this.fs.options.hook?.afterGet;
-          if (afterGet) {
-            await afterGet(this.path);
-          }
-        }
-        input.off("data", onData);
-        input.off("end", onEnd);
-        resolve(hash.digest("hex"));
-      };
-      input.on("end", onEnd);
-      const onError = (err: any) => {
-        input.off("data", onData);
-        input.off("error", onError);
-        reject(convertError(repository, path, err, false));
-      };
-      input.on("error", onError);
     });
   }
 
