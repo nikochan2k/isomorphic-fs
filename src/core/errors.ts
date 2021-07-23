74e74f94de031f52c69391cfa61cd4cdf82c5916
Yoshihiro Seki <nikochan2k@gmail.com>
2021-07-23 13:16:54 +0900

    add a SyntaxError

--- src/core/errors.ts
+++ src/core/errors.ts
@@ -51,10 +51,58 @@ export class NotFoundError extends AbstractFileSystemError {
   // #endregion Constructors (1)
 }
 
+export class SecurityError extends AbstractFileSystemError {
+  // #region Properties (1)
+
+  public name = "Security error";
+
+  // #endregion Properties (1)
+
+  // #region Constructors (1)
+
+  constructor(repository: string, path: string, detail?: any) {
+    super(repository, path, detail);
+  }
+
+  // #endregion Constructors (1)
+}
+
+export class AbortError extends AbstractFileSystemError {
+  // #region Properties (1)
+
+  public name = "Abort error";
+
+  // #endregion Properties (1)
+
+  // #region Constructors (1)
+
+  constructor(repository: string, path: string, detail?: any) {
+    super(repository, path, detail);
+  }
+
+  // #endregion Constructors (1)
+}
+
 export class NotReadableError extends AbstractFileSystemError {
   // #region Properties (1)
 
   public name = "Not readable error";
+
+  // #endregion Properties (1)
+
+  // #region Constructors (1)
+
+  constructor(repository: string, path: string, detail?: any) {
+    super(repository, path, detail);
+  }
+
+  // #endregion Constructors (1)
+}
+
+export class EncodingError extends AbstractFileSystemError {
+  // #region Properties (1)
+
+  public name = "Encoding error";
 
   // #endregion Properties (1)
 
@@ -99,42 +147,10 @@ export class InvalidStateError extends AbstractFileSystemError {
   // #endregion Constructors (1)
 }
 
-export class EncodingError extends AbstractFileSystemError {
+export class SyntaxError extends AbstractFileSystemError {
   // #region Properties (1)
 
-  public name = "Encoding error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
-
-  constructor(repository: string, path: string, detail?: any) {
-    super(repository, path, detail);
-  }
-
-  // #endregion Constructors (1)
-}
-
-export class QuotaExceededError extends AbstractFileSystemError {
-  // #region Properties (1)
-
-  public name = "Quota exceeded error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
-
-  constructor(repository: string, path: string, detail?: any) {
-    super(repository, path, detail);
-  }
-
-  // #endregion Constructors (1)
-}
-
-export class SecurityError extends AbstractFileSystemError {
-  // #region Properties (1)
-
-  public name = "Security error";
+  public name = "Syntax error";
 
   // #endregion Properties (1)
 
@@ -151,6 +167,22 @@ export class InvalidModificationError extends AbstractFileSystemError {
   // #region Properties (1)
 
   public name = "Invalid modification error";
+
+  // #endregion Properties (1)
+
+  // #region Constructors (1)
+
+  constructor(repository: string, path: string, detail?: any) {
+    super(repository, path, detail);
+  }
+
+  // #endregion Constructors (1)
+}
+
+export class QuotaExceededError extends AbstractFileSystemError {
+  // #region Properties (1)
+
+  public name = "Quota exceeded error";
 
   // #endregion Properties (1)
 
