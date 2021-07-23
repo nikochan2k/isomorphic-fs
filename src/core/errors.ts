297f47db64055e3b05d61a8a2777430e034caf4f
Yoshihiro Seki <nikochan2k@gmail.com>
2021-07-23 15:46:28 +0900

    add NotSupportedError

--- src/core/errors.ts
+++ src/core/errors.ts
@@ -1,13 +1,7 @@
 export abstract class AbstractFileSystemError implements Error {
-  // #region Properties (3)
-
   public message: string;
   public abstract name: string;
   public stack?: string;
-
-  // #endregion Properties (3)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail: any) {
     if (repository === "/") {
@@ -31,198 +25,108 @@ export abstract class AbstractFileSystemError implements Error {
       this.message += detail;
     }
   }
-
-  // #endregion Constructors (1)
 }
 
 export class NotFoundError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Not found error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class SecurityError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Security error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class AbortError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Abort error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class NotReadableError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Not readable error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class EncodingError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Encoding error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class NoModificationAllowedError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "No modification allowed error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class InvalidStateError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Invalid state error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class SyntaxError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Syntax error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class InvalidModificationError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Invalid modification error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class QuotaExceededError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Quota exceeded error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class TypeMismatchError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Type mismatch error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
-
-  // #endregion Constructors (1)
 }
 
 export class PathExistsError extends AbstractFileSystemError {
-  // #region Properties (1)
-
   public name = "Path exists error";
-
-  // #endregion Properties (1)
-
-  // #region Constructors (1)
 
   constructor(repository: string, path: string, detail?: any) {
     super(repository, path, detail);
   }
+}
 
-  // #endregion Constructors (1)
+export class NotSupportedError extends AbstractFileSystemError {
+  public name = "Not supported error";
+
+  constructor(repository: string, path: string, detail?: any) {
+    super(repository, path, detail);
+  }
 }
