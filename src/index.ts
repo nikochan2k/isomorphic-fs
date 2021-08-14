7efab2586dffec349d3221f5ecacc06d722f15f1
Yoshihiro Seki <nikochan2k@gmail.com>
2021-08-14 21:08:52 +0900

    fix index.ts bug

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "tslib";
 import "./polyfill";
 export * from "./core";
-export * as binary from "./util/Converter";
+export * as Converter from "./util/Converter";
 export * as misc from "./util/misc";
 export * as path from "./util/path";
