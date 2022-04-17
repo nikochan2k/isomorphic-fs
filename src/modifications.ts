9f88f20b795f2767f543b27b7a7e6a50d45aabb2
Yoshihiro Seki <nikochan2k@gmail.com>
2022-04-17 21:59:17 +0900

    add createModifiedReadableStream

--- src/modifications.ts
+++ src/modifications.ts
@@ -1,5 +1,5 @@
 import { Readable } from "stream";
-import { Data, DEFAULT_CONVERTER } from "univ-conv";
+import { closeStream, Data, DEFAULT_CONVERTER } from "univ-conv";
 
 export interface Modification {
   data: Data;
@@ -8,7 +8,7 @@ export interface Modification {
 }
 
 export class ModifiedReadable extends Readable {
-  private index = 0;
+  private iStart = 0;
   private mods: Modification[];
 
   constructor(private src: Readable, ...mods: Modification[]) {
@@ -22,14 +22,13 @@ export class ModifiedReadable extends Readable {
       const chunk = value as Uint8Array;
       const size = chunk.byteLength;
       try {
-        const iStart = this.index;
-        const iEnd = this.index + chunk.byteLength;
+        const iEnd = this.iStart + size;
         for (const mod of this.mods) {
           const mStart = mod.start;
           const mEnd =
             mod.start +
             (mod.length == null ? Number.MAX_SAFE_INTEGER : mod.length);
-          if (mStart <= iStart && iEnd <= mEnd) {
+          if (mStart <= this.iStart && iEnd <= mEnd) {
             /*
             (10)
             chunk:   |---|
@@ -44,11 +43,11 @@ export class ModifiedReadable extends Readable {
             chunk: |----|
             mod  : |-------|
             */
-            const start = iStart - mStart;
+            const start = this.iStart - mStart;
             const length = size;
             this.push(DEFAULT_CONVERTER.slice(mod.data, { start, length }));
             return;
-          } else if (iStart <= mStart && mStart <= iEnd) {
+          } else if (this.iStart <= mStart && mStart <= iEnd) {
             /*
             (1)
             chunk: |-------|
@@ -63,7 +62,7 @@ export class ModifiedReadable extends Readable {
             chunk: |-------|
             mod  : |----|
             */
-            const chunkLen = mStart - iStart;
+            const chunkLen = mStart - this.iStart;
             if (0 < chunkLen) {
               /*
               (1)
@@ -104,13 +103,13 @@ export class ModifiedReadable extends Readable {
                 chunk: |-------|
                 mod  : |----|
                 */
-                const start = mEnd - iStart;
+                const start = mEnd - this.iStart;
                 const length = iEnd - mEnd;
                 this.push(chunk.slice(start, length));
               }
             }
             return;
-          } else if (mStart < iStart && iStart < mEnd) {
+          } else if (mStart < this.iStart && this.iStart < mEnd) {
             /*
             (7)
             chunk:    |-------|
@@ -122,7 +121,7 @@ export class ModifiedReadable extends Readable {
             chunk:    |--|
             mod  : |-------|
             */
-            const start = iStart - mStart;
+            const start = this.iStart - mStart;
             let length: number;
             if (mEnd < iEnd) {
               /*
@@ -130,7 +129,7 @@ export class ModifiedReadable extends Readable {
               chunk:    |-------|
               mod  : |-------|
               */
-              length = mEnd - iStart;
+              length = mEnd - this.iStart;
             } else {
               /*
               (8)
@@ -140,7 +139,7 @@ export class ModifiedReadable extends Readable {
               chunk:    |--|
               mod  : |-------|
               */
-              length = iEnd - iStart;
+              length = iEnd - this.iStart;
             }
             this.push(DEFAULT_CONVERTER.slice(mod.data, { start, length }));
             return;
@@ -149,7 +148,7 @@ export class ModifiedReadable extends Readable {
           this.push(chunk);
         }
       } finally {
-        this.index += size;
+        this.iStart += size;
       }
     };
 
@@ -168,4 +167,164 @@ export class ModifiedReadable extends Readable {
   public override _read() {
     // noop
   }
+}
+
+export function createModifiedReadableStream(
+  src: ReadableStream<Uint8Array>,
+  ...mods: Modification[]
+) {
+  const reader = src.getReader();
+  return new ReadableStream({
+    start: async (controller) => {
+      let iStart = 0;
+      let done: boolean;
+      do {
+        const res = await reader.read();
+        const value = res.value;
+        done = res.done;
+        if (!done) {
+          const chunk = value as Uint8Array;
+          const size = chunk.byteLength;
+          try {
+            const iEnd = iStart + size;
+            for (const mod of mods) {
+              const mStart = mod.start;
+              const mEnd = mod.start + (mod.length ?? Number.MAX_SAFE_INTEGER);
+              if (mStart <= iStart && iEnd <= mEnd) {
+                /*
+                (10)
+                chunk:   |---|
+                mod  : |-------|
+                (11)
+                chunk:    |----|
+                mod  : |-------|
+                (12)
+                chunk: |-------|
+                mod  : |-------|
+                (13)
+                chunk: |----|
+                mod  : |-------|
+                */
+                const start = iStart - mStart;
+                const length = size;
+                controller.enqueue(
+                  DEFAULT_CONVERTER.slice(mod.data, { start, length })
+                );
+                return;
+              } else if (iStart <= mStart && mStart <= iEnd) {
+                /*
+                (1)
+                chunk: |-------|
+                mod  :    |-------|
+                (2)
+                chunk: |-------|
+                mod  :    |----|
+                (3)
+                chunk: |-------|
+                mod  :    |--|
+                (4)
+                chunk: |-------|
+                mod  : |----|
+                */
+                const chunkLen = mStart - iStart;
+                if (0 < chunkLen) {
+                  /*
+                  (1)
+                  chunk: |-------|
+                  mod  :    |-------|
+                  (2)
+                  chunk: |-------|
+                  mod  :    |----|
+                  (3)
+                  chunk: |-------|
+                  mod  :    |--|
+                  */
+                  controller.enqueue(chunk.slice(0, chunkLen));
+                }
+
+                if (mEnd < iEnd) {
+                  /*
+                  (3)
+                  chunk: |-------|
+                  mod  :    |--|
+                  (4)
+                  chunk: |-------|
+                  mod  : |----|
+                  */
+                  const modLen = mEnd - mStart;
+                  controller.enqueue(
+                    DEFAULT_CONVERTER.slice(mod.data, {
+                      start: 0,
+                      length: modLen,
+                    })
+                  );
+                  if (mEnd < iEnd) {
+                    /*
+                    (3)
+                    chunk: |-------|
+                    mod  :    |--|
+                    (4)
+                    chunk: |-------|
+                    mod  : |----|
+                    */
+                    const start = mEnd - iStart;
+                    const length = iEnd - mEnd;
+                    controller.enqueue(chunk.slice(start, length));
+                  }
+                }
+                return;
+              } else if (mStart < iStart && iStart < mEnd) {
+                /*
+                (7)
+                chunk:    |-------|
+                mod  : |-------|
+                (8)
+                chunk:    |----|
+                mod  : |-------|
+                (9)
+                chunk:    |--|
+                mod  : |-------|
+                */
+                const start = iStart - mStart;
+                let length: number;
+                if (mEnd < iEnd) {
+                  /*
+                  (7)
+                  chunk:    |-------|
+                  mod  : |-------|
+                  */
+                  length = mEnd - iStart;
+                } else {
+                  /*
+                  (8)
+                  chunk:    |----|
+                  mod  : |-------|
+                  (9)
+                  chunk:    |--|
+                  mod  : |-------|
+                  */
+                  length = iEnd - iStart;
+                }
+                controller.enqueue(
+                  DEFAULT_CONVERTER.slice(mod.data, { start, length })
+                );
+                return;
+              }
+
+              controller.enqueue(chunk);
+            }
+          } finally {
+            iStart += size;
+          }
+        }
+      } while (!done);
+      controller.close();
+      reader.releaseLock();
+      closeStream(src);
+    },
+    cancel: (e) => {
+      reader.releaseLock();
+      closeStream(src, e);
+    },
+  });
 }
